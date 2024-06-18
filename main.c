d8e5dca538fa636269833a06c51c72b318469dd6
ishay320 <ishay320@hotmail.com>
2024-06-18 21:44:41 +0300

    feat(push-pull): WIP push pull model

--- main.c
+++ main.c
@@ -7,14 +7,28 @@
 #include "raylib.h"
 #include "raymath.h"
 
+/**
+ * @description 2D pull 1 point to another.
+ *
+ * @param puller the stationary one, the one who pules
+ * @param pullee the one in motion, the one who gets pull
+ * @param force pull force
+ * @return the velocity of the pull that needs to apply
+ */
+Vector2 Vector2PullVel(const Vector2 puller, const Vector2 pullee, float force)
+{
+    Vector2 direction           = Vector2Subtract(puller, pullee);
+    Vector2 direction_normalize = Vector2Normalize(direction);
+    Vector2 vel                 = Vector2Scale(direction_normalize, force);
+
+    return vel;
+}
+
 struct payload {
     char *name;
 
-    float pos_x;
-    float pos_y;
-
-    float vel_x;
-    float vel_y;
+    Vector2 pos;
+    Vector2 vel;
 };
 
 struct node {
@@ -48,12 +62,11 @@ void destroy_graph(struct node *node)
 
 void graph_draw(struct node *root)
 {
-    DrawCircle(root->payload.pos_x, root->payload.pos_y, 5, RED);
+    DrawCircleV(root->payload.pos, 5, RED);
     for (size_t i = 0; i < root->len; i++) {
-        DrawLine(root->payload.pos_x, root->payload.pos_y,
-                 root->data[i]->payload.pos_x, root->data[i]->payload.pos_y,
-                 BLUE);
-        graph_draw(root->data[i]);
+        struct node *child = root->data[i];
+        DrawLineV(root->payload.pos, child->payload.pos, BLUE);
+        graph_draw(child);
     }
 }
 
@@ -64,33 +77,27 @@ Vector2 middle_of_screen()
 
 void graph_update(struct node *root)
 {
-    const float scale = 0.00001;
+    const float scale         = 0.01;
+    const float keep_distance = 60;
+    const float decay         = 0.9;
 
     for (size_t i = 0; i < root->len; i++) {
-        root->payload.vel_x +=
-            (root->data[i]->payload.pos_x - root->payload.pos_x) * scale;
-        root->payload.vel_y +=
-            (root->data[i]->payload.pos_y - root->payload.pos_y) * scale;
+        struct payload *a = &root->payload;
+        struct payload *b = &root->data[i]->payload;
 
-        root->data[i]->payload.vel_x +=
-            (root->payload.pos_x - root->data[i]->payload.pos_x) * scale;
-        root->data[i]->payload.vel_y +=
-            (root->payload.pos_y - root->data[i]->payload.pos_y) * scale;
+        const float force = Vector2Distance(b->pos, a->pos) - keep_distance;
+        Vector2 a_vel     = Vector2PullVel(b->pos, a->pos, force * scale);
+        Vector2 b_vel     = Vector2PullVel(a->pos, b->pos, force * scale);
+
+        // also multiply decay
+        a->vel = Vector2Scale(Vector2Add(a->vel, a_vel), decay);
+        b->vel = Vector2Scale(Vector2Add(a->vel, b_vel), decay);
+
         graph_update(root->data[i]);
     }
 
-    // pull to the middle of the screen
-    Vector2 middle = middle_of_screen();
-    root->payload.vel_x += (middle.x - root->payload.pos_x) * scale;
-    root->payload.vel_y += (middle.y - root->payload.pos_y) * scale;
-
-    // friction
-    root->payload.vel_x -= root->payload.vel_x * 0.001;
-    root->payload.vel_y -= root->payload.vel_y * 0.001;
-
-    // update position using velocity
-    root->payload.pos_x += root->payload.vel_x;
-    root->payload.pos_y += root->payload.vel_y;
+    // update position of the node
+    root->payload.pos = Vector2Add(root->payload.pos, root->payload.vel);
 }
 
 void add_random_node(struct node *root, size_t count)
@@ -100,8 +107,8 @@ void add_random_node(struct node *root, size_t count)
     for (size_t i = 0; i < count; i++) {
         char *node_name = malloc(15);
         sprintf(node_name, "rand - %ld", num++);
-        node_push(root, (struct payload){node_name, rand() % 500, rand() % 500,
-                                         0, 0});
+        node_push(root, (struct payload){
+                            node_name, {rand() % 500, rand() % 500}, {0, 0}});
     }
 }
 
@@ -113,8 +120,8 @@ struct node_list {
 
 void graph_to_list(struct node *root, struct node_list *node_list)
 {
+    da_push(*node_list, root);
     for (size_t i = 0; i < root->len; i++) {
-        da_push(*node_list, root->data[i]);
         graph_to_list(root->data[i], node_list);
     }
 }
@@ -122,28 +129,34 @@ void graph_to_list(struct node *root, struct node_list *node_list)
 void graph_push(struct node *root)
 {
     // TODO: in progress
+    const float scale = 1;
+    const float decay = 0.4;
+
     struct node_list node_list = {0};
-    da_push(node_list, root);
     graph_to_list(root, &node_list);
-    {
-        size_t i = 0;
-        // for (size_t i = 0; i < node_list.len; i++) {
+
+    for (size_t i = 0; i < node_list.len; i++) {
         for (size_t j = 0; j < node_list.len; j++) {
             if (i == j) {
                 continue;
             }
-            Vector2 d =
-                Vector2Subtract((Vector2){node_list.data[i]->payload.pos_x,
-                                          node_list.data[i]->payload.pos_y},
-                                (Vector2){node_list.data[j]->payload.pos_x,
-                                          node_list.data[j]->payload.pos_y});
-            char t[24];
-            sprintf(t, "%f,%f", 1 / (d.x * 0.1), 1 / (d.y * 0.1));
-            DrawText(t, node_list.data[j]->payload.pos_x,
-                     node_list.data[j]->payload.pos_y, 12, BLACK);
-            node_list.data[j]->payload.pos_x += 1 / (d.x * 0.1);
-            node_list.data[j]->payload.pos_y += 1 / (d.y * 0.1);
+
+            struct payload *a = &node_list.data[j]->payload;
+            struct payload *b = &node_list.data[i]->payload;
+
+            const float force =
+                25 / (Vector2Distance(b->pos, a->pos) * 0.4 + 2);
+            Vector2 a_vel = Vector2PullVel(b->pos, a->pos, force * scale);
+            Vector2 b_vel = Vector2PullVel(a->pos, b->pos, force * scale);
+
+            // also multiply decay
+            a->vel = Vector2Scale(Vector2Subtract(a->vel, a_vel), decay);
+            b->vel = Vector2Scale(Vector2Subtract(b->vel, b_vel), decay);
         }
+    }
+    for (size_t i = 0; i < node_list.len; i++) {
+        struct node *node = node_list.data[i];
+        node->payload.pos = Vector2Add(node->payload.pos, node->payload.vel);
     }
 }
 
@@ -152,7 +165,7 @@ int main(void)
     const int screen_width  = 800;
     const int screen_height = 450;
 
-    struct node *root = node_create((struct payload){"foo", 50, 30, 0, 0});
+    struct node *root = node_create((struct payload){"foo", {50, 30}, {0, 0}});
     add_random_node(root, 10);
     add_random_node(root->data[0], 10);
 
@@ -165,11 +178,18 @@ int main(void)
         ClearBackground(RAYWHITE);
         DrawFPS(10, 10);
 
-        graph_update(root);
-        // graph_push(root);
-        graph_draw(root);
+        {  // pull root to the middle of the screen
+            const float scale = 0.1;
+            Vector2 middle    = middle_of_screen();
+            const float force = Vector2Distance(middle, root->payload.pos);
+            Vector2 a_vel =
+                Vector2PullVel(middle, root->payload.pos, force * scale);
+            root->payload.vel = a_vel;
+        }
 
-        DrawCircleV(middle_of_screen(), 2, BLACK);
+        graph_update(root);
+        graph_push(root);
+        graph_draw(root);
 
         EndDrawing();
     }
